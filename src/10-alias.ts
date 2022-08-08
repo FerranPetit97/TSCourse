9662f0b1245915e2113be5dc9de919c3e952c8f7
Ferran Petit <fpmartiwork@gmail.com>
2022-08-08 21:05:23 +0200

    Second part

--- src/10-alias.ts
+++ src/10-alias.ts
@@ -1,10 +1,19 @@
 (() => {
-  type UserID = string | number | boolean;
+  type UserID = string | number;
   let userId: UserID;
+  // Literal types
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+  let shirtSize: Sizes;
+  shirtSize = 'M';
+  shirtSize = 'L';
+  shirtSize = 'XL';
+  shirtSize = 'S';
 
-  function greeting(userId: UserID) {
+  function greeting(userId: UserID, size: Sizes) {
     if (typeof userId === 'string') {
       console.log(`string ${userId.toLowerCase()}`);
     }
   }
+
+  greeting(1111, 'XL');
 })();
